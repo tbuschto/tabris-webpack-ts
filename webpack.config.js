504e0fb6449119e92cad8bb955c5e3cb3df62dc7
Tim Buschtoens <tbuschto@eclipsesource.com>
2021-05-11 18:08:34 +0200

    Configure source map support

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ module.exports = {
   // Make sure the entry file is correct for your app:
   entry: './src/index.ts',
   externals: ['tabris', 'tabris-decorators'],
+  devtool: 'inline-source-map',
+  mode: 'development',
   output: {
     libraryTarget: 'commonjs2',
     filename: 'index.js',
-    path: __dirname + '/dist'
+    path: __dirname + '/dist',
+    devtoolModuleFilenameTemplate: '../[resource-path]',
+    devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]'
   },
   // If you don't use any TypeScript code, you don't need to include the extensions
   // ".ts" and ".tsx" and can also omit the "module" configuration below.
